36eb96667b4f8ec340912d2c62e70938ec83ea1b
Jayne Doe <jayne.doe@ibm.com>
2020-02-03 16:58:55 +0000

    feat(yo): start on the questions needed for generation

---
+++ generator/app/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  constructor (args, opts) {
+    super(args, opts);
+  }
+
+  async prompting () {
+    this.setup = await this.prompt([
+      {
+        type: 'input',
+        name: 'project_name',
+        message: 'What is the name of your project?',
+      },
+      {
+        type: 'confirm',
+        name: 'enable_http2',
+        message: 'Do you want to enable HTTP/2?',
+      },
+      {
+        type: 'confirm',
+        name: 'enable_rendering',
+        message: 'Will you be rendering any HTML?',
+      },
+    ]);
+  }
+};
