6bd9faad4d0bf948ca649c4a012939e2af2aa618
Jayne Doe <jayne.doe@ibm.com>
2020-12-07 16:04:25 +0000

    fix(govuk): fix width styling for govuk templates
    Signed-off-by: Jayne Doe <jayne.doe@ibm.com>

--- generators/build-env/templates/config/compile.config.js
+++ generators/build-env/templates/config/compile.config.js
@@ -1,6 +1,11 @@
 const metaPackage = require('../package.json');
 
 module.exports = {
+<% if (base_engine === 'GovUK') { -%>
+  banner: {
+    phrase: 'Internal',
+  },
+<% } -%>
   base_path: '',
   metadata: metaPackage,
 };
